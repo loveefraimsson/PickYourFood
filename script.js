3b3ac7fd6225c7afd8e0b81ff88be9c35fbbe4d3
loveefraimsson <loveefraimsson@gmail.com>
2021-03-25 16:28:57 +0100

    Prints categories

---
+++ script.js
@@ -0,0 +1,29 @@
+let main = document.getElementById("main");
+let categoryContainer = document.getElementById("categoryContainer");
+let infoContainer = document.getElementById("infoContainer");
+
+let categoryArray = [];
+
+let categoryList = document.createElement("UL");
+categoryContainer.appendChild(categoryList);
+
+
+fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+.then(response => response.json())
+.then(data => {
+    printCategories(data);
+})
+
+function printCategories(data) {
+    for (category in data.categories) {      
+        categoryArray.push(data.categories[category].strCategory);      
+    }
+    categoryArray.sort();
+
+    for (i = 0; i < categoryArray.length; i++) {
+        categoryContainer.insertAdjacentHTML("beforeend", "<li>" + categoryArray[i] + "</li>");
+    }
+
+}
+
+//FILTER BY CATEGORIES - https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
